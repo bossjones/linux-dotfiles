cef388f6c6d9f64a3f3a7c2d048366c4c911045d
Malcolm Jones <malcolm@adobe.com>
2018-01-22 01:59:27 -0500

    chg: update ansible run

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -2,5 +2,5 @@
 
 cd ~/.dotfiles/ansible
 ansible-galaxy install -r requirements.txt --roles-path roles/ -vvv
-echo "localhost ansible_connection=local" > hosts.private
-ansible-playbook main.yml --ask-pass --ask-become-pass
+echo "localhost ansible_connection=local ansible_python_interpreter=/usr/bin/python2" > hosts.private
+ansible-playbook bootstrap_xenial.yml --ask-pass --ask-become-pass
