9cf9bae76d4bd6159b3ff9c6df6e004edc9e5f89
Malcolm Jones <malcolm@adobe.com>
2018-02-03 13:45:53 -0500

    chg: skip tags for nvm temporarily

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -4,5 +4,5 @@ pushd ~/.dotfiles/ansible
 ansible-galaxy install -r requirements.yml --roles-path roles/ -vvv
 echo "localhost ansible_connection=local ansible_python_interpreter=/usr/bin/python2" > hosts.private
 ansible-playbook bootstrap_xenial.yml
-ansible-playbook install_version_managers.yml
+ansible-playbook install_version_managers.yml --skip-tags="nvm"
 popd
