b9b55a5667d48437ecdca20f9668b8413f7c4d8d
Malcolm Jones <malcolm@adobe.com>
2018-02-13 22:25:38 -0500

    Chg: prereq etc, some osx support

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
+unamestr=$(uname)
+
+if [[ $unamestr == "Darwin" ]]; then
+  _PATH_TO_PYTHON="/usr/bin/python"
+elif [[ $unamestr == "Linux"  && -f $(which apt-get) ]]; then
+  _PATH_TO_PYTHON="/usr/bin/python2"
+fi
+
 pushd ~/.dotfiles/ansible
 ansible-galaxy install -r requirements.yml --roles-path roles/ -vvv
-echo "localhost ansible_connection=local ansible_python_interpreter=/usr/bin/python2" > hosts.private
-ansible-playbook bootstrap_xenial.yml
+echo "localhost ansible_connection=local ansible_python_interpreter=${_PATH_TO_PYTHON}" > hosts.private
+
+
+# Mac OS X
+if [[ $unamestr == "Darwin" ]]; then
+  ansible-playbook bootstrap_osx.yml
+elif [[ $unamestr == "Linux"  && -f $(which apt-get) ]]; then
+  ansible-playbook bootstrap_xenial.yml
+fi
+
+# TODO: Add variable files for different machines
 # ansible-playbook install_version_managers.yml --skip-tags="nvm"
 ansible-playbook install_version_managers.yml
 popd
