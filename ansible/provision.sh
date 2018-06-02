6b1641501e30015964660d593bdf1863f4b51a33
Malcolm Jones <malcolm@adobe.com>
2018-06-02 09:50:47 -0400

    wip: manifest

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -45,7 +45,7 @@ fi
 # ansible-playbook install_version_managers.yml --skip-tags="nvm"
 if [[ $unamestr == "Darwin" ]]; then
   _USER=$(whoami)
-  _GROUP=$(whoami)
+  _GROUP="staff"
 
   # If we set checkonly then run check, else run full suite
   if [[ "${CHECK_ONLY}" = "1" ]]; then
