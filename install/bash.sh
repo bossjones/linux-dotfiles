0997a5e141f8708efc056b97b12a62e03c5e36e0
Malcolm Jones <malcolm@adobe.com>
2018-06-03 18:12:22 -0400

    wip: manifest

--- install/bash.sh
+++ install/bash.sh
@@ -11,4 +11,4 @@ if [[ "${_TRAVIS_CI}" == "1" ]]; then
 fi
 
 grep "/usr/local/bin/bash" /private/etc/shells &>/dev/null || sudo bash -c "echo /usr/local/bin/bash >> /private/etc/shells"
-chsh -s /usr/local/bin/bash
+sudo chsh -s /usr/local/bin/bash
