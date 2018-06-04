23de0bca7b767940743a4af093bb79991b3c43b8
Malcolm Jones <malcolm@adobe.com>
2018-06-03 21:55:59 -0400

    Fix: sudo -v

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# NOTE: 6/3/2018 newly added
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Get current dir (so run this script from anywhere)
 
 export DOTFILES_DIR DOTFILES_CACHE DOTFILES_EXTRA_DIR
