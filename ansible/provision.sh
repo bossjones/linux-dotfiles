85e20875beb673f6f1bb03bcbfa600ade105b8e9
Malcolm Jones <malcolm@adobe.com>
2018-06-03 20:52:46 -0400

    Feature: sudo timeout fix

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -14,6 +14,18 @@ elif [[ $unamestr == "Linux"  && -f $(which dnf) ]]; then
   _PATH_TO_PYTHON="/usr/bin/python2"
 fi
 
+
+# Warn that some commands will not be run if the script is not run as root.
+if [[ $EUID -ne 0 ]]; then
+  RUN_AS_ROOT=false
+  printf "Certain commands will not be run without sudo privileges. To run as root, run the same command prepended with 'sudo', for example: $ sudo $0\n\n" | fold -s -w 80
+else
+  RUN_AS_ROOT=true
+  # Update existing `sudo` timestamp until `.osx` has finished
+  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+fi
+
+
 # OVERRIDES: Use relative directory if travis
 if [[ "${_TRAVIS_CI}" == "1" ]]; then
     _PATH_TO_PYTHON=`which python`
