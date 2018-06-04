234088b92b854f3c9ca1d524c929318182074009
Malcolm Jones <malcolm@adobe.com>
2018-06-03 22:11:26 -0400

    wip: manifest

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-# NOTE: 6/3/2018 newly added
-# Ask for the administrator password upfront.
-sudo -v
+# OVERRIDES: Use relative directory if travis
+if [[ "${_TRAVIS_CI}x" == "x" ]]; then
+  # NOTE: 6/3/2018 newly added
+  # Ask for the administrator password upfront.
+  sudo -v
+fi
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
