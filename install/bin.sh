a09e43ee5aa422bcdede673e94924b67e4efc0d0
Malcolm Jones <bossjones@theblacktonystark.com>
2020-12-10 16:37:14 -0500

    WIP: exponential_backoff

--- install/bin.sh
+++ install/bin.sh
@@ -12,6 +12,7 @@ chmod +x $(PWD)/bin/autocaption
 chmod +x $(PWD)/bin/autotrim
 chmod +x $(PWD)/bin/smartcrop
 chmod +x $(PWD)/bin/open-browser-generic
+chmod +x $(PWD)/bin/exponential_backoff
 
 
 ln -sfv $(PWD)/bin/fzf* ~/bin/
@@ -25,3 +26,4 @@ ln -sfv $(PWD)/bin/autocaption ~/bin/
 ln -sfv $(PWD)/bin/autotrim ~/bin/
 ln -sfv $(PWD)/bin/smartcrop ~/bin/
 ln -sfv $(PWD)/bin/open-browser-generic ~/bin/
+ln -sfv $(PWD)/bin/exponential_backoff ~/bin/
