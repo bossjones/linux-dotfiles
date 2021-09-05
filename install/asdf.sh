77de621e2ac6bf4fa2ed83be6798e88ca2dc04db
Malcolm Jones (bossjones/Tony Dark) <bossjones@theblacktonystark.com>
2021-09-05 18:44:32 -0400

    wip: fixed ansible conditionals (#30)

--- install/asdf.sh
+++ install/asdf.sh
@@ -5,23 +5,23 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0 || true
 echo "[asdf] enable"
 . $HOME/.asdf/asdf.sh
 
-# asdf plugin-add 1password https://github.com/samtgarson/asdf-1password.git # 1.6.0 
-# asdf plugin add goss https://github.com/raimon49/asdf-goss.git # 0.3.13 
+# asdf plugin-add 1password https://github.com/samtgarson/asdf-1password.git # 1.6.0
+# asdf plugin add goss https://github.com/raimon49/asdf-goss.git # 0.3.13
 asdf plugin-add hadolint https://github.com/looztra/asdf-hadolint # 1.18.0
-asdf plugin add fd # 8.1.1 
+asdf plugin add fd # 8.1.1
 asdf plugin-add tmux https://github.com/aphecetche/asdf-tmux.git # 2.9a
-asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git # 3.3.1 
+asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git # 3.3.1
 asdf plugin-add jsonnet https://github.com/Banno/asdf-jsonnet.git # 0.16.0
-asdf plugin-add k9s https://github.com/looztra/asdf-k9s # 0.21.7 
+asdf plugin-add k9s https://github.com/looztra/asdf-k9s # 0.21.7
 asdf plugin-add kubectl https://github.com/Banno/asdf-kubectl.git # 1.18.6
 asdf plugin add kubectx # 0.9.1
-asdf plugin-add kubeval https://github.com/stefansedich/asdf-kubeval # 0.15.0 
-asdf plugin-add neovim # 0.4.4 
+asdf plugin-add kubeval https://github.com/stefansedich/asdf-kubeval # 0.15.0
+asdf plugin-add neovim # 0.4.4
 asdf plugin-add packer https://github.com/Banno/asdf-hashicorp.git # 1.6.2
 asdf plugin-add terraform https://github.com/Banno/asdf-hashicorp.git # 0.13.2
 asdf plugin-add vault https://github.com/Banno/asdf-hashicorp.git # 1.5.3
 asdf plugin-add poetry https://github.com/crflynn/asdf-poetry.git # 1.0.10
-asdf plugin-add yq https://github.com/sudermanjr/asdf-yq.git # 3.2.3 
+asdf plugin-add yq https://github.com/sudermanjr/asdf-yq.git # 3.2.3
 
 # asdf install goss 0.3.13
 # asdf global goss 0.3.13
@@ -47,8 +47,8 @@ asdf global kubectl 1.18.6
 asdf install kubectx 0.9.1
 asdf global kubectx 0.9.1
 
-asdf install kubeval 0.15.0 
-asdf global kubeval 0.15.0 
+asdf install kubeval 0.15.0
+asdf global kubeval 0.15.0
 
 asdf install neovim 0.4.4
 asdf global neovim 0.4.4
@@ -65,8 +65,8 @@ asdf global vault 1.5.3
 asdf install poetry 1.0.10
 asdf global poetry 1.0.10
 
-asdf install yq 3.2.3
-asdf global yq 3.2.3
+asdf install yq v4.5.0
+asdf global yq v4.5.0
 
 echo " [fd] testing"
 fd --help
